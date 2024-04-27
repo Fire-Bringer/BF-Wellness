2a159b4599a17b82d987d07b40d410f37e0f882a
Rashad DuPaty <radupaty@gmail.com>
2024-04-27 14:17:10 +0900

    added info within the new config.rb file

--- config/initializers/cloudinary.rb
+++
@@ -1,9 +0,0 @@
-# config/initializers/cloudinary.rb
-
-cloudinary_url = URI.parse(ENV['CLOUDINARY_URL'])
-Cloudinary.config do |config|
-  config.cloud_name = cloudinary_url.host
-  config.api_key = cloudinary_url.user
-  config.api_secret = cloudinary_url.password
-  config.secure = true # Optionally, enable secure HTTPS URLs
-end
